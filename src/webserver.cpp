25843d529926602d12301a98d0d6a72223594aed
Brice Aldrich <aldrbw01@students.ipfw.edu>
2017-01-31 16:35:12 +0000

    More clean up.

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -189,33 +189,32 @@ void setup_server(void) {
     int ret;
 
     struct pico_ip4 address = {0};
-    port = short_be(serv.port); // Have to use their short be thing...
+    port = short_be(conf.getPort()); // Have to use their short be thing...
 
     listen_socket = pico_socket_open(PICO_PROTO_IPV4, PICO_PROTO_TCP,
             &cb_tcpserver);
 
     if (!listen_socket) {
-        printf("Could not open socket!\n");
+        printf("[ERROR:194] =======> Could not open socket. FATAL!!!\n");
         exit(1);
     }
-    log_debug("Socket successfully opened.");
+
+    log_debug("[DEBUG:194] =======> Socket successfully opened");
 
     ret = pico_socket_bind(listen_socket, &address, &port);
     if (ret < 0) {
-        printf("could not bind socket\n");
+        printf("[ERROR:204] =======> Could not bind to socket. FATAL!!!\n");
         exit(1);
     }
-    std::string s;
-    s.append("Socket successfully bound to port ");
-    s.append(std::to_string(port));
-    log_debug(s);
+
+    log_debug("[DEBUG:204] =======> Socket successfully bound to port " + std::to_string(port));
 
     ret = pico_socket_listen(listen_socket, 40);
     if (ret < 0) {
-        printf("could not listen\n");
+        printf("[ERROR:212] =======> Could not listen to socket. FATAL!!!\n");
         exit(1);
     }
-    log_debug("Successfully listening on socket.");
+    log_debug("[DEBUG:212} =======> Successfully listening on socket.");
 }
 
 struct pico_device* init_picotcp(void) {
